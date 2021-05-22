e56e3b40feb21c10784660a4a6209fa3647783d9
vraj141 <vrajshah141@gmail.com>
2021-05-22 13:38:34 +0530

    update

---
+++ build/precache-manifest.5445378ddd8535d0ea0ed4df07940cef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "844d21d54538ba957b6d",
+    "url": "/pdftron-viewer-react/static/css/main.8cd34642.chunk.css"
+  },
+  {
+    "revision": "844d21d54538ba957b6d",
+    "url": "/pdftron-viewer-react/static/js/main.844d21d5.chunk.js"
+  },
+  {
+    "revision": "5d60e93fcf3361f2fc7f",
+    "url": "/pdftron-viewer-react/static/js/1.5d60e93f.chunk.js"
+  },
+  {
+    "revision": "65bc4df79e5796e59657",
+    "url": "/pdftron-viewer-react/static/js/runtime~main.65bc4df7.js"
+  },
+  {
+    "revision": "f9ad4b2f6f3af9daaff9f69e149333a2",
+    "url": "/pdftron-viewer-react/index.html"
+  }
+];
